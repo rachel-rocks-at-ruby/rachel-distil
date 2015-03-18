18b6eaa20e28a40597d6be5ca5f0b7119218cd4d
vagrant <rachelb@gmail.com>
2015-03-18 23:10:54 +0000

    Updated Readme and added IP address to domains

---
+++ README.md
@@ -0,0 +1,5 @@
+#Rachel Bird's technical assessment for the Ruby Developer position at Distil Networks
+
+This Ruby on Rails application uses Ruby on Rails 4+ and Ruby 2+.
+
+https://rachel-distil.herokuapp.com/
--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -10,6 +10,7 @@ class DomainsController < ApplicationController
 
 	def create
 		@domain = Domain.new(params.require(:domain).permit(:hostname, :ip, :account_id))
+		@domain.ip = `host #{@domain.hostname}`.match(/(\d{1,3}\.){3}\d{1,3}/).to_s
 
 		if @domain.save
       redirect_to account_path(@domain.account), notice: "Domain was created successfully."
